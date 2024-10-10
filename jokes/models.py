c082afd2d5781ea015aef3e24fa5e1a45c5f0194
Quanesha <quanesha_pickens@yahoo.com>
2024-10-10 18:56:26 -0500

    Adding Properties to the Joke Model

--- jokes/models.py
+++ jokes/models.py
@@ -20,6 +20,18 @@ class Joke(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    @property
+    def num_votes(self):
+        return self.jokevotes.count()
+
+    @property
+    def num_likes(self):
+        return self.jokevotes.filter(vote=1).count()
+
+    @property
+    def num_dislikes(self):
+        return self.jokevotes.filter(vote=-1).count()
+
     def get_absolute_url(self):
         return reverse('jokes:detail', args=[self.slug])
 
@@ -99,4 +111,3 @@ class JokeVote(models.Model):
                 fields=['user', 'joke'], name='one_vote_per_user_per_joke'
             )
         ]
-        
